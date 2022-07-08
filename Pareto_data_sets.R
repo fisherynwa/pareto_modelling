a63c300f494ca9550b8130e1cc8e872e160e50a3
Vladimir Vutov <vkvutov@rtg09.math.uni-bremen.de>
2022-07-08 12:35:55 +0200

    initial commit

---
+++ Pareto_data_sets.R
@@ -0,0 +1,68 @@
+#Univariate pareto
+
+# (1) bilbao waves data set in the paper "Efficient likelihood based inference 
+#for the generalized Pareto distribution"
+
+install.packages("ercv")
+library(ercv)
+bilbao		
+
+#(2) Actuarial science data from page 13 of the paper; The Kumaraswamy Pareto IV Distribution.
+#The data set corresponding to lifetimes (in years) of retired women with temporary
+#disabilities, which are incorporated in the Mexican insurance public system and who died
+#during 2004, 
+
+
+
+#(3) Annual maximum temperatures data from page 13 of the paper; The Kumaraswamy Pareto IV Distribution.
+
+#The data describe annual maximum temperatures at Oxford and Worthing (England), for
+#the period 1901 to 1980. The data are: 
+
+y=c(75, 89, 84, 84, 85, 81, 92, 79, 84, 85, 79, 95, 87,
+83, 87, 82, 83, 89, 84, 86, 79, 89, 86, 93, 86, 85, 85, 
+80, 87, 87, 89, 95, 89, 86, 86, 83, 87,84, 84, 85, 89, 
+88, 92, 87, 84, 84, 90, 90, 89, 86, 82, 89, 90, 83, 87, 
+82, 86, 80, 91, 82, 87,77, 81, 87, 81, 80, 83, 89, 88,
+88, 84, 77, 85, 77, 91, 94, 80, 80, 85, 83)
+y
+
+#The data is also available from R-achieve http://www.R-project.org.
+
+
+# (4) Sum of skin folds data from page 13 of the paper; The Kumaraswamy Pareto IV Distribution.
+
+#This data set shows 102 male and 100 female athletes collected at the Australian Institute of Sports, 
+
+y1=c(28.0, 109.1, 102.8, 104.6, 126.4, 80.3, 75.2, 87.2, 97.9, 75.1, 65.1, 171.1,
+76.8, 117.8, 90.2, 97.2, 99.9, 125.9, 69.9, 98, 96.8, 80.3, 74.9, 83.0, 91.0, 76.2, 52.6, 111.1,
+110.7, 74.7, 113.5, 99.8, 80.3, 109.5, 123.6, 91.2, 49.0, 110.2, 89.0, 98.3, 122.1, 90.4, 106.9,
+156.6, 101.1, 126.4, 114.0, 70.0, 77.0, 148.9, 80.1, 156.6, 115.9, 181.7, 71.6, 143.5, 200.8, 68.9,
+103.6, 71.3, 54.6, 88.2, 95.4, 47.5, 55.6, 62.9, 52.5, 62.6, 49.9, 57.9, 109.6, 98.5, 136.3, 103.6,
+102.8, 131.9, 33.8, 43.5, 46.2, 73.9, 36.8, 67, 41.1, 59.4, 48.4, 50.0, 54.6, 42.3, 46.1, 46.3, 109.0,
+98.1, 80.6, 68.3, 47.6, 61.9, 38.2, 43.5, 56.8, 41.6, 58.9, 44.5, 41.8, 33.7, 50.9, 40.5, 51.2, 54.4,
+52.3, 57.0, 65.3, 52.0, 42.7, 35.2, 49.2, 61.8, 46.5, 34.8, 60.2, 48.1, 44.5, 54.0, 44.7, 64.9, 43.8,
+58.3, 52.8, 43.1, 78.0, 40.8, 41.5, 50.9, 49.6, 88.9, 48.3, 61.8, 43.0, 61.1, 43.8, 54.2, 41.8, 34.1,
+30.5, 34.0, 46.7, 71.1, 65.9, 34.3, 34.6, 31.8, 34.5, 31.0, 32.6, 31.5, 32.6, 31.0, 33.7, 30.3, 38.0,
+55.7, 37.5, 112.5, 82.7, 29.7, 38.9, 44.8, 30.9, 44.0, 37.5, 37.6, 31.7, 36.6, 48, 41.9, 30.9, 52.8,
+43.2, 113.5, 96.9, 49.3, 42.3, 96.3, 56.5, 105.7, 100.7, 56.8, 75.9, 52.8, 47.8, 76.0, 61.2, 75.6,
+43.3, 49.5, 70.0, 75.7, 57.7, 67.2, 56.5, 47.6, 60.4, 34.9)
+y1
+
+
+# (5) Glass fiber data on page 3 of the paper; 
+#A Comparison of Maximum Likelihood and Bayesian Estimators for the Three-Parameter Weibull Distribution.
+
+
+
+
+
+
+#Multivariate pareto
+
+#(1) The paper titled; On Bayesian approach to composite Pareto models by 
+#Muhammad Hilmi Abdul MajidID*, Kamarulzaman Ibrahim. However, these
+#data can be requested online from https://www.dosm.gov.my/v1/index.php.). There are more
+#than 12 000 observations in each dataset, representing the households in Malaysia.
+
+(2)
